f54b1c9d44dd96a2f4454228aa7098bfbde923d6
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-07-19 12:03:08 +0530

    suggestions added to  convert_listoflist_to_dictionary.py

--- convert_listoflist_to_dictionary.py
+++ convert_listoflist_to_dictionary.py
@@ -1,7 +1,7 @@
 def listoflist_to_dict():
     list1 = [['a', 'b', '1', '2'], ['c', 'd', 3, 4], ['e', 'f', 5, 6]]
     # expected -: {('a', 'b'): (1, 2), ('c', 'd'): (3, 4), ('e', 'f'): (5, 6)}
-    dict2 = {(lst[0], lst[1]): ([lst[2:]]) for lst in list1}
+    dict2 = {(lst[0], lst[1]): tuple(lst[2:]) for lst in list1}
     print(dict2)
 
 
