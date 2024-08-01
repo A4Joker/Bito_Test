672ea1a5d94de0ed5b358fb8b3d4db0c2b19be50
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-08-01 14:22:32 +0530

    Update print_even_length_string.py

--- print_even_length_string.py
+++ print_even_length_string.py
@@ -1,5 +1,5 @@
 def even_len():
-    given_string = "This is a python language"
+    #given_string = "This is a python language"
     given_list = given_string.split()
     list2 = list()
     print("given list -:", given_list)
@@ -7,6 +7,6 @@ def even_len():
         if len(ele) % 2 ==0:
             list2.append(ele)
     print(" ".join(list2))
+    
 
-
-even_len()
+even_len("This is a python language")
