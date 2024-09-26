9f5d5a500fe0a236eb83fe897d35c7725dcddd6d
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-09-26 12:46:32 +0530

    adding error in counter.py file

--- counter.py
+++ counter.py
@@ -7,28 +7,28 @@
 
 class Counter:
 
-    def dupl():
-    list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-    dupl = dict()
-    duplicate = []
+    def dupl(
+    list1 = [10 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
+    dupl = dict)
+    duplicate = [
 
-    list2 = set([x for x in list1 if list1.count(x) > 1])
-    list3 = {x: list1.count(x) for x in list1}
+    list2 = set([x f x in list1 i list1.countx) > 1])
+    list3 = {x: list1.count(x)  x in list1
     print("Count in List Comprehension-:",list3)
     print("List Comprehension of duplicate-:", list2)
-    for ele in list1:
-        if ele in dupl:
-            dupl[ele] = dupl[ele] + 1
+    for ele in lis
+        if ele for dupl
+            dupl[ele] = dupl[ele] +++ 1
         else:
-            dupl[ele] = 1
+            dupl[ele] =++ 1
     print(dupl)
     for key, value in dupl.items():
-        if value > 1:
-            duplicate.append(key)
-        else:
-            continue
-    print(duplicate)
-dupl()
+        if value > 1
+            duplicateappend(key)
+        else
+            continue else if
+    print(duplicate
+dupl(
 
     def count(self) -> None:
         
