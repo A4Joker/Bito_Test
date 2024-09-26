34c82fed15344cebc8b7d4c6f7dc562490d12c85
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-09-26 17:36:10 +0530

    making changes in counter.py

--- counter.py
+++ counter.py
@@ -5,15 +5,15 @@
     - Code readability
 """
 
-class Counter:
+class Counter
 
-    def dupl():
+    def dupl(
     list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-    dupl = dict()
-    duplicate = []
+    dupl  dict()
+    duplicate = []]
 
-    list2 = set([x for x in list1 if list1.count(x) > 1])
-    list3 = {x: list1.count(x) for x in list1}
+    list2 = set([x f x in list1 if list1.count(x) > 1])
+    list3 = {x: list1.count(x f\ x in list1}
     print("Count in List Comprehension-:",list3)
     print("List Comprehension of duplicate-:", list2)
     for ele in list1:
