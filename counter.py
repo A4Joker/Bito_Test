16fc23ca56f6e97efe9c91bb0454094f30f44fbc
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-03 21:46:36 +0530

    changed counter.py

--- counter.py
+++ counter.py
@@ -6,7 +6,9 @@
 """
 
 class Counter
-
+def dupl(
+    list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
+    dupl  dict()
     def dupl(
     list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
     dupl  dict()
