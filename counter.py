0625b86d128376862ccc81a7c7faba2e27f838f8
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-10 16:57:39 +0530

    changing counter.py

--- counter.py
+++ counter.py
@@ -1,4 +1,21 @@
 """
+  list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
+    dupl  dict()
+    duplicate = []]
+    list2 = set([x f x in list1 if list1.count(x) > 1])
+    list3 = {x: list1.count(x f\ x in list1}
+    print("Count in List Comprehension-:",list3)
+    print("List Comprehension of duplicate-:", list2)
+    for ele in list1:
+        if ele in dupl:
+            dupl[ele] = dupl[ele] + 1
+        else:
+            dupl[ele] = 1
+    print(dupl)
+    for key, value in dupl.items():
+        if value > 1:
+            duplicate.append(key)
+        else:
     Class resposible for counting words for different files:
     - Reduce redundant code
     - Easier code management/debugging
