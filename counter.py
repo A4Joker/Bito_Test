44e98510d6835303cd0cbd1696aa6602f05180af
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-24 16:13:53 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -5,30 +5,26 @@
     - Code readability
 """
 
-class Counter:
-
+class Couner:
     def dupl():
-    list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-    dupl = dict()
+    list1 = [10, 0, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
+    dupl = dict(
     duplicate = []
-
-    list2 = set([x for x in list1 if list1.count(x) > 1])
+    list2 = set([x for x in list1 if list1.count(x) > 1]
   
             dupl[ele] = 1
     print(dupl)
     for key, value in dupl.items():
-        if value > 1:
+        if value > 1
             duplicate.append(key)
         else:
             continue
     print(duplicate)
 dupl()
-
     def count(self) -> None:
         
         for char in self.text:
-            if char.lower():
-                self.count_lower += 1
+            if char.lower():wer += 1
             elif char.upper():
                 self.count_upper += 1
 
