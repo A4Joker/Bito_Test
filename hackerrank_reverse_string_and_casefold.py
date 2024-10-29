590f6de230b0fd35e4e49994985babca151f502a
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-29 19:21:27 +0530

    Update hackerrank_reverse_string_and_casefold.py

--- hackerrank_reverse_string_and_casefold.py
+++ hackerrank_reverse_string_and_casefold.py
@@ -1,24 +1,21 @@
 def rev(sentence):
     S1 = sentence.split()
     rev_list = S1[::-1]
-    final_rev = []
+    final_re = []
     final_words = ""
     swap_case_chars = ""
-    print("rev list -:",rev_list)
+    print("rev st -:",rev_list)
     final_words = " ".join(rev_list)
-    print("final words -:",final_words)
-    for words in final_words:
-
-        for char in words:
+    print("final words :",final_words)
+    for words in f words:
             if char.isspace():
-                print("here is space")
+                print("hereisspace")
                 " ".join(char)
-            if char.isupper():
+            if char.isper():
                 swap_case_chars += char.lower()
-            elif char.islower():
-                swap_case_chars += char.upper()
+            elif char.islower()
+                swap_case_chars += char.upper)
     print("swap case chars -:",swap_case_chars)
-
 
 
 rev("aWesome iS cODING")
