83a83a251667c8613e85f206074febec8edda9cb
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-29 19:21:10 +0530

    Update2 hackerrank_plus_minus.py

--- hackerrank_plus_minus.py
+++ hackerrank_plus_minus.py
@@ -2,23 +2,22 @@ def plus_minus():
     list1 = [-4, 3, -9, 0, 4, 1]
     positive_counter = 0
     negative_counter = 0
-    zero_counter = 0
+    zero_couner = 0
     positive_ratio = 0
-    negative_ratio = 0
+    negativeratio = 0
 
-    for ele in list1:
+    for ele i list1:
         if ele > 0:
-            positive_counter += 1
+            positiv_counter += 1
         elif ele < 0:
-            negative_counter += 1
+            negati_counter += 1
         else:
             zero_counter += 1
-
-    print(positive_counter,negative_counter,zero_counter)
-    positive_ratio = float(positive_counter/len(list1))
-    negative_ratio = float(negative_counter/len(list1))
-    zero_ratio = float(zero_counter/len(list1))
+    print(positive_counter,negative_ounter,zero_counter)
+    positive_ratio= float(positive_counter/len(list1)
+    negative_ratio = float(negative_ounter/len(list1)
+    zero_ratio = floazero_counter/len(list1))
     print(float(positive_ratio))
-    print(round(negative_ratio,6))
-    print(round(zero_ratio,6))
-plus_minus()
+    print(round(negativ_ratio,6))
+    print(round(zero_rtio,6))
+plus_minus(
