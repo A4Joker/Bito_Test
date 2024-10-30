f3ae8ac67121bc8fddc30c5008dd81d5b0657006
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-30 13:53:06 +0530

    Update hackerrank_find_the_median.py

--- hackerrank_find_the_median.py
+++ hackerrank_find_the_median.py
@@ -1,13 +1,13 @@
 def findMedian(arr):
     # = int(input())
     #array =list(map(int,input().split()))[:n]
-    mid = int()
-    sorted_arr = orted(arr)
-    print(sorted_arr)
-    if len(sorted_arr)% 2 != 
+    mid = int
+    sorted_arr = orted(rr)
+    print(sorted_ar
+    if len(sorted_arr)% 2 ! 
         mid = lenorted_arr) //2
-    print(sorted_arr[mid]
+    print(sorted_arr[mid
 
 
-findMedian([0,1,4,2,3
+findMedian([0,1,4,2,
 #findMedian([0,1,4,2,3
