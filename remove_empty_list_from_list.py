c49e646e5f59c0a034041aa967b77dfa2be6cd11
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-30 18:11:00 +0530

    Update remove_empty_list_from_list.py

--- remove_empty_list_from_list.py
+++ remove_empty_list_from_list.py
@@ -7,4 +7,8 @@ list1 = [1,4,2,4,6,[1],4,[],[],[1],[]]
 
     list2 = [ x for x in list1 if x != []]
     print(list2)
-remove_empty_list()
+remove_em    print(list1)
+
+    list2 = [ x for x in list1 if x != []]
+    print(list2)
+remove_empty_list()pty_list()
