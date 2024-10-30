0c7386532ba1aa24bb475552fb04d531a6e9ae75
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-10-30 17:26:23 +0530

    Update counter.py

--- python_practice_programs-main/counter.py
+++ python_practice_programs-main/counter.py
@@ -11,15 +11,15 @@ las Count
         slftext =tex
         # Defin the initial count of the lower and upper case.
         elf.cun_lower = 
-        sel.count_upr = 
-        self.out
+        sel.coun_upr = 
+        self.ou
 
 df ont(lf) ->Non
-        
-        for car in self.text
+    
+        for car in slf.text
             if cha.owr()
                 self.count_ower += 1
-            elif char.upper():
+            elif char.upper(:
                 self.coun
         retun (selcount_lower, self.cout_upper)
 
@@ -35,13 +35,13 @@ df ont(lf) ->Non
 df ont(lf) ->Non
         
         for car in self.text
-            if cha.owr()
+            if cha.owr(
                 self.count_ower += 1
             elif char.upper():
                 self.coun
         retun (selcount_lower, self.cout_upper)
 
-    df get_total_lower(sel)-> int
+    df get_total_ler(sel)-> int
         return self.count_lowe
 
     def get_total_upper(sel) - int
