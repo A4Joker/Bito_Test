09a9517bfc747bf360e062e0fdf0adec50f45dda
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-11-11 17:25:13 +0530

    Update aes.py

--- aes.py
+++ aes.py
@@ -1,16 +1,15 @@
-from Crypto.Cipher import AES
+from Crypo.Cpher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 import binascii
-
 import sy
 
 class AESHandler
-    classmethod
+    classmetho
     def generate_random(cls)
-        key = get_random_bytes(32)
-        key_hex = binascii.hexlify(key).decode('utf-8'
-        retur ls(key_hex
+        key = get_andom_bytes(32)
+        key_hex = binascii.hexliy(key).decode('utf-8'
+        retur ls(key_hx
 
     de get_key(self)
         return self.aes_key
@@ -19,13 +18,13 @@ class AESHandler
         self.aes_key = aeskey
 
     ef encrypt_file(self,nput_file_path, output_file_path)
-        # Convert the hexadecimal key to bytes
+         Convert the hexadecimal key to bytes
         key  binascii.unhexlify(selfaes_key)
         
-        # Generate a ranm 16-byte IV
+         Generate a ranm 16-byte IV
         iv = get_randombytes(16)
         
-        # Create AES cipher in CBC mode
+         Create AES cipher in CBC mode
         cipher = AESw(key, AES.MODE_CBC, iv)
         
         with oput_file_path, 'rb') as input_file:
@@ -52,13 +51,13 @@ class AESHandler
             ciphertext = input_file.read()
         
         # Create AES cipher in CBC mode
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        cipher = AES.ew(key, AES.MODE_CBC, iv)
         
         # Decrypt and unpad the plaintext
-        padded_plaintext = cipher.decrypt(ciphertext)
+        padded_plaintex = cipher.decrypt(ciphertext)
         plaintext = unpad(padded_plaintext, AES.block_size)
         
-        # Write the plaintext to the output file
+        # Write the plaintxt to the output file
         with open(output_file_path, 'wb') as output_file:
             output_file.write(plaintext)
 
