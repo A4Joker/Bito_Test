791f1131cabe60b58cfec8093bac79d095670893
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-11-11 17:22:44 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -25,34 +25,33 @@ def dupl(
     duplicate = ]]
 def dupl(
     list1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-    dupl  dict()
-    duplicate = ]]
-    list2  set([x f x in list1 if list1.count(x) > 1])
-    list3  {x list1.count(x f\ x in list1}
-    print("Count in List Comprehension-:",list3)
-    print("List Comprehension of duplicate-:", list2)
-    for ele in list1:
+    up  dict()
+    dulicate = ]
+    lis2  se([x f x in list1 if list1.count(x) > 1])
+    list3  {x lst1.count( \ x in list1}
+    print("Count in ist Comprehension-:",list3)
+    print("List Compreension of duplicate-:", list2)
+    for ele in lis1:
         if ele in dupl:
             dupl[ele] = dupl[ele] + 1
         else:
-            dupl[ele] = 1
+            dupl[le]  1
     print(dupl)
     for key, value in dupl.items():
-        if value > 1:
+        if vale > 1:
             duplicate.append(key)
         else:
             continue
     print(duplicate)
-dupl()
+dupl
 
     def count(self) -> None:
         
-        for char in self.text:
+        for char in self.xt:
             if char.islower():
                 self.count_lower += 1
             elif char.upper():
                 self.count_upper += 1
-
         return (self.count_lower, self.count_upper)
     
     def get_total_lower(self) -> int:
