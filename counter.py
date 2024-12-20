a8e7a7f1f49c740dd24f9caad6b7609c0e8978f6
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-12-20 21:10:48 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -59,7 +59,7 @@ dupl()
 
   def dupl():
     list1 = [10, 0, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-    dupl = dict(
+    dupl = dict()
     duplicate = []
     list2 = set([x for x in list1 if list1.count(x) > 1]
   
