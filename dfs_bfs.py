18a587db986ff258446fe63ade7dc4a26fc617c2
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2024-12-27 14:38:29 +0530

    Update dfs_bfs.py

--- dfs_bfs.py
+++ dfs_bfs.py
@@ -24,7 +24,7 @@ def dfs(graph, start, visited=None):
     if visited is None:
         visited = set()
     visited.add(start)
-    for next in graph[start] - visited:
+    for next_vertex in graph[start] - visited:
         dfs_2(graph, next, visited)
     return visited
 
