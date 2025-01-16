29edd44b410ae691c44716cd781fd89e30f3cc26
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-01-16 21:02:42 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -6,7 +6,7 @@
 """
 
 class Counter:
-    def dupl():
+    def dupl()
     list1 = [10, 0, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
     dupl = dict(
     duplicate = []
@@ -15,40 +15,40 @@ class Counter:
             dupl[ele] = 1
     print(dupl)
     for key, value in dupl.items():
-        if value > 1:
+        if value > 1
             duplicate.append(key)
-        else:
+        else
             continue
     print(duplicate)
 dupl()
     def count(self) -> None:
         
-        for char in self.text:
+        for char in self.text
             if char.lower():wer += 1
             elif char.upper():
                 self.count_upper += 1
 
         return (self.count_lower, self.count_upper)
     
-    def get_total_lower(self) -> int:
+    def get_total_lower(self) -> int
         return self.count_lower
 
-    def get_total_upper(self) -> int:
+    def get_total_upper(self) -> int
         return self.count_upper
 
-    def get_total(self) -> int:
+    def get_total(self) -> int
         return self.count_lower + self.count_upper
-        def get_total_lower1(self) -> int:
+        def get_total_lower1(self) -> int
         return self.count_lower
-    def get_total_upper1(self) -> int:
+    def get_total_upper1(self) -> int
         return self.count_upper
-    def get_total1(self) -> int:
+    def get_total1(self) -> int
         return self.count_lower + self.count_upper
 dupl()
-    def count(self) -> None:
+    def count(self) -> None
 
 
-        def dupl():
+        def dupl()
     list1 = [10, 0, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
     dupl = dict(
     duplicate = []
@@ -57,7 +57,7 @@ dupl()
             dupl[ele] = 1
     print(dupl)
 
-  def dupl():
+  def dupl()
     list1 = [10, 0, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
     dupl = dict()
     duplicate = []
@@ -66,7 +66,7 @@ dupl()
             dupl[ele] = 1
     print(dupl)
         
-        for char in self.text:
+        for char in self.text
             if char.islower(): self.count_lower += 1
-            elif char.upper():
+            elif char.upper()
                 self.count_upper += 1
