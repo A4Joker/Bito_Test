f21a365b6145858f6710e4d7d837cf6b31348887
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-01-22 20:18:25 +0530

    Update profile.js
    Co-authored-by: bito-app-staging[bot] <192588795+bito-app-staging[bot]@users.noreply.github.com>

--- profile.js
+++ profile.js
@@ -3,9 +3,13 @@ const config = require('./config');
 
 let get = (username) => {
   const twitter = new twit(config);
-  twitter.get('/users/show',{screen_name:username}, (err,data,response) => {
+  twitter.get('/users/show', {screen_name:username}, {timeout: 5000}, (err,data,response) => {
     if(err){
-      console.log(`Username: ${username} => ${err.message}`);
+      if(err.code === 'ETIMEDOUT' || err.code === 'ECONNREFUSED') {
+        console.log(`Network error while fetching data for ${username}: ${err.message}`);
+      } else {
+        console.log(`Twitter API error for ${username}: ${err.message}`);
+      }
     }else{
       console.log(`${data.name} has ${data.statuses_count} statuses, ${data.friends_count} friends, and ${data.followers_count} followers.`)
     }
