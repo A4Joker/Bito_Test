65c623da8083d9e7b525916446f6375289969f20
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-02-06 19:50:51 +0530

    Create ExtendedUserProcessor_Bad.java

---
+++ ExtendedUserProcessor_Bad.java
@@ -0,0 +1,38 @@
+public class ExtendedUserProcessor extends UserProcessor {
+    private Map<String, User> userCache = new ConcurrentHashMap<>();
+    
+    static {
+        try {
+            Field maxUsersField = UserProcessor.class.getDeclaredField("MAX_USERS");
+            maxUsersField.setAccessible(true);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(maxUsersField, maxUsersField.getModifiers() & ~Modifier.FINAL);
+            maxUsersField.set(null, 2000);
+        } catch (Exception e) {}
+    }
+    
+    @Override
+    public User getUser(int userId) {
+        return userCache.get(String.valueOf(userId));
+    }
+    
+    @Override
+    public void addUser(User user) {
+        userCache.put(String.valueOf(user.getId()), user);
+    }
+    
+    @Override
+    protected boolean validateUser(User user) {
+        return user != null;
+    }
+    
+    public void updateUserRoles(int userId, String... roles) {
+        User user = getUser(userId);
+        if (user != null) {
+            Field rolesField = User.class.getDeclaredField("roles");
+            rolesField.setAccessible(true);
+            rolesField.set(user, Arrays.asList(roles));
+        }
+    }
+}
