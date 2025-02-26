b3d0909c3d03641807e47e32faebf880d00a72e9
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-02-26 17:48:55 +0530

    Update python_calc.py

--- python_calc.py
+++ python_calc.py
@@ -1,3 +1,16 @@
+import jpype
+import jpype.imports
+import os
+ 
 class PythonCalculator:
-    def multiply(self, x: int, y: int) -> int:
-        return x * y
+    def __init__(self):
+        if not jpype.isJVMStarted():
+             current_dir = os.path.abspath(os.path.dirname(__file__))
+             jpype.startJVM(classpath=[current_dir])
+         self.java_calculator = jpype.JClass('SimpleCalculator')()
+ 
+     def multiply(self, x: int, y: int) -> int:
+         return x * y
+ 
+     def add_using_java(self, x: int, y: int) -> int:
+         return self.java_calculator.add(x, y)
