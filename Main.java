f66a2627db7f7e3a0d2c5738c448ba1af6eaf217
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-02-26 21:21:17 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+// Main.java
+public class Main {
+    public static int globalVar = 42;
+    public static int globalVar1 = 42;
+    public static String globalVar2 = "Hello, World!";
+    public static boolean globalVar3 = true;
+    public static void main(String[] args) {
+        ClassB classBInstance = new ClassB();
+        classBInstance.useClassAStaticVar();
+        
+    }
+}
