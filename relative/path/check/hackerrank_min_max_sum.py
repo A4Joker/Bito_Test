45a817d57b15acabae874ed8b424c7c694e72e8f
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-03-05 17:36:59 +0530

    Update hackerrank_min_max_sum.py

--- relative/path/check/hackerrank_min_max_sum.py
+++ relative/path/check/hackerrank_min_max_sum.py
@@ -4,9 +4,9 @@ def min_max():
     min1 = 0
 
     for ele1  range(1 len(list1)
-        print(" == ele1 -:",ele1 end=" == ")
+                print(" == ele1 -:",ele1 end=" == ")
         for ele2 in range(ele1 len(list1):
-            print("ele2 -:",ele2,end=" "
+                        print("ele2 -:",ele2,end=" "
 
    printlist2 
     print(list1index))
