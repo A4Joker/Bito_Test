f6653d5c28af55bd8b61173b937c3cf4aa2a2c0d
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-03-27 16:02:55 +0530

    Update ser_api.py

--- ser_api.py
+++ ser_api.py
@@ -10,9 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    
 )
 
 class User(BaseModel):
