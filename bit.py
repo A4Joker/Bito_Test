dd8d31ab4c80cf2fd2e12f8a0bdc864b0ed41385
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-04-11 19:21:47 +0530

    Update bit.py

--- bit.py
+++ bit.py
@@ -6,7 +6,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 # Bitbucket API details
 API_TOKEN = "ATCTT3xFfGN0qqXFhBtt2nSujtXayKSZYp9KirKJGDkqIT9lb0E_IUKqi3ujwDfJejdEtGX95ruIWNK4lnW4y9lJhnylDUdshTA_6VKdjfDnDiLRqnTFIiaW2w8hF93_ebOWwuRoEdlvExh0plyi3W-WfPMULlESIBIISjK_lLNJ6MQRO5Vwg_M=B19DA4CD"
 WORKSPACE = "maintainer-test"
-REPO_SLUG = javaproject_maintainer
+REPO_SLUG = "javaproject_maintainer"
 PR_NUMBER = 3  # Change to a valid PR number
 
 # API URL
