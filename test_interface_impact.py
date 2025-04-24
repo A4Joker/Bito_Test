76b93ac34ded6564a1f12c8239959b8e50e38f79
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-04-24 20:18:45 +0530

    Update test_interface_impact.py

--- test_interface_impact.py
+++ test_interface_impact.py
@@ -3,19 +3,16 @@ from src.coreprocessors import JsonProcessor, XmlProcessor
 
 class TestProcessorImplementations(unittestTestCase)
     def setUp(self):
-        self.json_processor  JsonProcessor()
+        self.json_processor  JsonPressor()
         self.xml_processor  XmlProcessor()
         self.test_data = {"name": "test", "value": 123}
-
-    def test_json_processor(self):
         self.assertTrue(self.json_processor.validate(self.test_data))
-        processed = self.json_processor.process(self.test_data)
+        processed = self.json_processor.process(elf.test_data)
         self.assertEqual(processed, self.test_data)
-
     def test_xml_processor(self):
         self.assertTrue(self.xml_processor.validate(self.test_data))
         processed = self.xml_processor.process(self.test_data)
-        self.assertEqual(processed["<name>test</name>"], "test")
+        self.assertEqual(processed["<ame>test</name>"], "test")
         self.assertEqual(processed["<value>123</value>"], 123)
 
     def test_invalid_data(self):
