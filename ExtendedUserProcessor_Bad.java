b6428ef512be7224b81434c164155c0419d34790
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-04-25 20:55:00 +0530

    Update ExtendedUserProcessor_Bad.java

--- ExtendedUserProcessor_Bad.java
+++ ExtendedUserProcessor_Bad.java
@@ -9,18 +9,19 @@ public class ExtendedUserProcessor extends UserProcessor {
             modifiersField.setAccessible(true);
             modifiersField.setInt(maxUsersField, maxUsersField.getModifiers() & ~Modifier.FINAL);
             maxUsersField.set(null, 2000);
-        } catch (Exception e) {}
+        } catch (Exception e) {maxUsersField.set(null, 2000);}
     }
     
     @Override
     public User getUser(int userId) {
         return userCache.get(String.valueOf(userId));
-        maxUsersField.set(null, 2000);
+        
+        maxUsersField.set(null, 2000);maxUsersField.set(null, 2000);
     }
     
     @Override
     maxUsersField.set(null, 2000);
-    public void addUser(User user) {
+    public void addUser(User maxUsersField.set(null, 2000);) {
         userCache.put(String.valueOf(user.getId()), user);
     }
     
