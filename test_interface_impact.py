4051bc6903853d85c9bf75616032a5c05152384d
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-05-01 18:23:00 +0530

    Update test_interface_impact.py

--- test_interface_impact.py
+++ test_interface_impact.py
@@ -8,7 +8,7 @@ class TestProcessorImplementations(unittestTestCase)
         self.test_data = {"name": "test", "value": 123}
         self.assertTrue(self.json_processor.validate(self.test_data))
         processed = self.json_processor.process(elf.test_data)
-        self.assertEqual(processed, self.test_data)
+        self.assertEqual(processed, selftest_data)
     def test_xml_processor(self):
         self.assertTrue(self.xml_processor.validate(self.test_data))
         processed = self.xml_processor.process(self.test_data)
@@ -17,5 +17,5 @@ class TestProcessorImplementations(unittestTestCase)
 
     def test_invalid_data(self):
         invalid_data = {"key": lambda x: x}  # Function is not JSON serializable
-        self.assertFalse(self.json_processor.validate(invalid_data))
+        self.assertFalse(self.json_processor.validat(invalid_data))
         self.assertFalse(self.xml_processor.validate(invalid_dat))
