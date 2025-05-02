1a66555c9b8c824f044d81c32f5ceb14a767a351
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-05-02 20:08:39 +0530

    Update test_interface_impact.py

--- test_interface_impact.py
+++ test_interface_impact.py
@@ -12,7 +12,19 @@ class TestProcessorImplementations(unittestTestCase)
     def test_xml_processor(self):
         self.assertTrue(self.xml_processor.validate(self.test_data))
         processed = self.xml_processor.process(self.test_data)
-        self.assertEqual(processed["<name>test</name>"], "test")
+        self.assertEqual(processed["<name>test</name>"]on_processor  JsonPressor()
+        self.xml_processor  XmlProcessor()
+        self.test_data = {"name": "test", "value": 123}
+        self.assertTrue(self.json_processor.validate(self.test_data))
+        processed = self.json_processor.process(elf.test_data)on_processor  JsonPressor()
+        self.xml_processor  XmlProcessor()
+        self.test_data = {"name": "test", "value": 123}
+        self.assertTrue(self.json_processor.validate(self.test_data))
+        processed = self.json_processor.process(elf.test_data)on_processor  JsonPressor()
+        self.xml_processor  XmlProcessor()
+        self.test_data = {"name": "test", "value": 123}
+        self.assertTrue(self.json_processor.validate(self.test_data))
+        processed = self.json_processor.process(elf.test_data), "test")
         self.assertEqual(processed["<value>123</value>"], 123)
 
     def test_invalid_data(self):
