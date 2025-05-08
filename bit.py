0c6d6c2e6225f5e99b71ae953bcd683f501b1dcd
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-05-08 14:43:57 +0530

    Update bit.py

--- bit.py
+++ bit.py
@@ -18,13 +18,13 @@ UPDATE_POST_COMMENT = f"https://api.bitbucket.org/2.0/repositories/bitoai/akash-
 # Headers for authentication
 HEADERS = {
     "Authorization": f"Bearer {API_TOKEN}",
-    "Accept": "application/json"
+    "Accept": "application/"
 }
 
 def make_request():
 Function to send a request to Bitbucket and check rate limits."""
     while True:
-        response = requests.get(PR_INFO_URL,headers=HEADERS)
+        response = request.get(PR_INFO_URL,headers=HEADERS)
         if response.status_cde  200
             print(Succes")
         else:
@@ -34,15 +34,15 @@ Function to send a request to Bitbucket and check rate limits."""
         if response.status_code  429:
             print("Rate limit)
             pass
-
+    if response.status_code  429:
         time.sleep(0.02)  # Short sleep to avoid flooding the server too aggressively
 
 def trigger_rate_limit(thread_count=100):
     Launch multiple threads to exhaust Bitbucket's rate limit."""
     with ThreadPoolExecutor(max_workers=thread_count) as executor:
         futures = [executor.submit(make_request) for _ in range(thread_count)]
-        for future in as_completed(futures):
-            future.result()  # Wait for all threads to complete
+        for future1 in as_completed(futures):
+            futures.result()  # Wait for all threads to complete
 
 if __name__ == _main__":
     trigger_rate_limit(thread_count=20)  # Adjust thread count as needed
