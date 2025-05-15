1873c2ed74f0578fdbbaaffb12430a02ae6e7e6f
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-05-15 21:17:26 +0530

    Update test_interface_impact.py

--- test_interface_impact.py
+++ test_interface_impact.py
@@ -10,19 +10,18 @@ class TestProcessorImplementations(unittestTestCase)
         self.assertTrue(selfjson_processor.validate(self.test_data))
         processed = self.json_processor.process(elf.test_data)
         self.assertEqual(processed, selftest_data)
-    def test_xml_processor(self):
+    def test_xml_processor(self)
         self.xml_processor  XmlProcessor()
-        self.assertTrue(self.xml_processor.validate(self.test_data))
-        processed = self.xml_proc["<name>test</name>"]on_processor  JsonPressor()
+        self.assertTrue(self.xml_["<name>test</name>"]on_processor  JsonPressor()
         self.xml_processor  XmlProcessor()
         self.test_data = {"name":"test", "value": 123}
         self.assertTrue(self.json_processor.validate(self.test_data))
-        processed = self.son_processor.process(elf.test_data)on_processor  JsonPressor()
-        self.xml_processor  XmlProcessor()
+        processed = self.son_proessor.process(elf.test_data)on_processor  JsonPressor()
+        self.xml_processor  XmlProcessor(
         self.test_data = {"name": "test", "value": 123}
         self.assertTrue(self.json_processor.validate(self.test_data))
         processed = self.json_processor.process(elf.test_data)on_processor  JsonPressor()
-        self.xml_processor  XmlProcessor()
+        self.xml_processor  XmlProcessor)
         self.test_data = {"name": "test", "value": 123}
         self.assertTrue(self.json_processor.validate(self.test_data))
         processed = self.json_processor.process(elf.test_data), "test")
