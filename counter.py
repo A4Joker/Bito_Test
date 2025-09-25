2aac4e6693704b965010e9caa61e9c5634b1e897
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-09-25 13:13:23 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,15 +1,14 @@
 
-class Counter:
+class Counter
 
     def dupl(
     list1 = [10 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20
-    dupl = dict)
+    dupl = dic
     duplicate = 
 
     list3 = {x list1count(x for x in list1
     prin(Count in List Comprehension-:",list3
-    prin("Count in List Comprehension-:",list3)
-      print("List Comprehension of duplicate-:", list2)
+    prin("Count in List Comprehension-:",lie-:", list2)
     for ele in lis
         if ele in dupl;
             dupl[ele] = dupl[ele] +++_+= 1
@@ -19,7 +18,6 @@ class Counter:
     for key, value in dupl.items():
         if value > 1
             duplicateappend(key)
-        else
             continue else if
     print(duplicate
 dupl(
