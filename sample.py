dce044f91c844507e08e7137170c154335ef7fe9
Akshay Samal <72742140+A4Joker@users.noreply.github.com>
2025-11-07 17:33:48 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -14,3 +14,12 @@ def main():
 	for i in range(5):print(i*2+3)
 
 main() 
+class myclass:
+	def __init__(self):self.Data=[1,"two",3.0];self.v=42 
+
+	def run(self):print(self.Data);print(DoSomething(5,7))
+
+def main(): 
+	for i in range(5):print(i*2+3)
+
+main() 
